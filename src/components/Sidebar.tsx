6cb8145d1022265af7545fbb4d8f0116f4931f51
Yasir Ado Hassan <ysquare.theimperial@gmail.com>
2023-03-22 23:12:10 +0100

    adding-sidebar-icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
-import {useLocation} from 'react-router-dom'
-import {GoHome} from 'react-icons/go'
-import {HiOutlineUsers} from 'react-icons/hi'
-import {FaUsers} from 'react-icons/fa'
+import { useLocation } from "react-router-dom";
+import { GoHome } from "react-icons/go";
+import { HiOutlineUsers } from "react-icons/hi";
+import { FaUsers, FaRegHandshake, FaUserCheck, FaUserTimes } from "react-icons/fa";
+import {TbMoneybag} from 'react-icons/tb'
+import {MdOutlineSavings} from 'react-icons/md'
+import {GiReceiveMoney} from 'react-icons/gi'
 export default function Sidebar() {
-    const location = useLocation()
+  const location = useLocation();
   return (
     <div className="sidebar">
       <p className="item_head mt-3">
         Switch Organization <IoIosArrowDown size="1.2rem" />
       </p>
-      <p className="sidebar_item mt-3"><GoHome size='1.2rem'/>{' '}Dashboard</p>
+      <p className="sidebar_item mt-3">
+        <GoHome size="1.2rem" /> Dashboard
+      </p>
       <div>
         <p className="item_head mt-3">CUSTOMERS</p>
         <div>
@@ -20,15 +25,17 @@ export default function Sidebar() {
               location.pathname === "/users" && "active_sidebar_item"
             }`}
           >
-            <HiOutlineUsers size='1.2rem'/>{' '}Users
+            <HiOutlineUsers size="1.2rem" /> Users
           </p>
-          <p className="sidebar_item"><FaUsers size='1.2rem'/>{' '}Guarantors</p>
-          <p className="sidebar_item">Loans</p>
-          <p className="sidebar_item">Decision Models</p>
-          <p className="sidebar_item">Savings</p>
-          <p className="sidebar_item">Loan Requests</p>
-          <p className="sidebar_item">White List</p>
-          <p className="sidebar_item">Karma</p>
+          <p className="sidebar_item">
+            <FaUsers size="1.2rem" /> Guarantors
+          </p>
+          <p className="sidebar_item"><TbMoneybag size='1.2rem'/>{' '}Loans</p>
+          <p className="sidebar_item"><FaRegHandshake size='1.2rem'/>{' '}Decision Models</p>
+          <p className="sidebar_item"><MdOutlineSavings size='1.2rem'/>{' '}Savings</p>
+          <p className="sidebar_item"><GiReceiveMoney size='1.2rem'/>{''}Loan Requests</p>
+          <p className="sidebar_item"><FaUserCheck size='1.2rem'/>{' '}White List</p>
+          <p className="sidebar_item"><FaUserTimes size='1.2rem'/>{' '}Karma</p>
         </div>
       </div>
       <div>
